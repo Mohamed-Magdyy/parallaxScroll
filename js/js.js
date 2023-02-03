e731b0bc836b3477e6fad809a0d245c7270d3ff4
Mohamed-Magdyy <mohamed01266109132@gmail.com>
2023-02-03 15:36:18 +0200

    first commit

---
+++ js/js.js
@@ -0,0 +1,42 @@
+gsap.from("#monutain1",{
+     scrollTrigger :{
+          scrub: true
+     },
+     y:100,
+})
+gsap.from("#monutain2",{
+     scrollTrigger :{
+          scrub: true
+     },
+     y:200,
+})
+gsap.from("#tree2",{
+     scrollTrigger :{
+          scrub: true
+     },
+     x:-50,
+})
+gsap.from("#tree1",{
+     scrollTrigger :{
+          scrub: true
+     },
+     x:-50,
+})
+gsap.from("#man",{
+     scrollTrigger :{
+          scrub: true
+     },
+     x:-250,
+})
+gsap.from("#plants",{
+     scrollTrigger :{
+          scrub: true
+     },
+     x:-50,
+})
+gsap.from("#text",{
+     scrollTrigger :{
+          scrub: true
+     },
+     x:400,
+})
